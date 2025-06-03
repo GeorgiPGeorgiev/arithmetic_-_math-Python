3ea62500786bcdbad1a790f89dc29d8c7d7e2996
GeorgiPGeorgiev <143788637+GeorgiPGeorgiev@users.noreply.github.com>
2025-06-04 00:48:19 +0300

    Delete arithmetic_&_math directory

--- arithmetic_&_math/area_of_circle.py
+++
@@ -1,7 +0,0 @@
-import math
-
-radius = float(input("Enter the radius of a circle: "))
-
-area = round(math.pi,2) * radius ** 2
-
-print(f"The circumference is: {area}cm^2")
--- arithmetic_&_math/circumference_of_circle.py
+++
@@ -1,7 +0,0 @@
-import math
-
-radius = float(input("Enter the radius of a circle: "))
-
-circumference = 2 * round(math.pi,2) * radius
-
-print(f"The circumference is: {circumference}cm")
