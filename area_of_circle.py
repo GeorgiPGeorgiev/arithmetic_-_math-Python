67aa23d6b0610374892aa126f5bb008dca70a962
GeorgiPGeorgiev <143788637+GeorgiPGeorgiev@users.noreply.github.com>
2025-06-04 00:48:46 +0300

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Enter the radius of a circle: "))
+
+area = round(math.pi,2) * radius ** 2
+
+print(f"The circumference is: {area}cm^2")
---
+++ library_functions.py
@@ -0,0 +1,26 @@
+import math
+
+print("pi")
+print(math.pi)
+print()
+
+print("e")
+print(math.e)
+print()
+
+result = math.sqrt(9)
+print("sqrt")
+print(result)
+print()
+
+#rounds up
+result = math.ceil(9.15)
+print("ceil")
+print(result)
+print()
+
+#rounds down
+result = math.floor(9.99)
+print("floor")
+print(result)
+print()
