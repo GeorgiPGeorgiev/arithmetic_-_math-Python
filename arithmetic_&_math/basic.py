157262c4dcef0e5ed82fe036ef7fbdb4f40bb24e
GeorgiPGeorgiev <143788637+GeorgiPGeorgiev@users.noreply.github.com>
2025-06-04 00:46:23 +0300

    Add files via upload

---
+++ arithmetic_&_math/basic.py
@@ -0,0 +1,44 @@
+
+friends = 5
+
+#friends = friends + 1
+friends += 1
+print("+")
+print(friends)
+print()
+friends = 5
+
+#friends = friends - 2
+friends -= 2
+print("-")
+print(friends)
+print()
+friends = 5
+
+#friends = friends * 3
+friends *= 3
+print("*")
+print(friends)
+print()
+friends = 5
+
+#friends = friends / 2
+friends /= 2
+print("/")
+print(friends)
+print()
+friends = 5
+
+#friends = friends ** 2
+friends **= 2
+print("**")
+print(friends)
+print()
+friends = 5
+
+#friends = friends % 2
+friends %= 2
+print("%")
+print(friends)
+print()
+friends = 5
